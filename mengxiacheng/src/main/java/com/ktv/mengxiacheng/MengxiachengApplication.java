7f7c3160ae2299a4a9bebcdd7fec3210c2b36318
yangj55 <603678227@qq.com>
2018-01-31 14:51:15 +0800

    第一次提交

---
+++ mengxiacheng/src/main/java/com/ktv/mengxiacheng/MengxiachengApplication.java
@@ -0,0 +1,12 @@
+package com.ktv.mengxiacheng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MengxiachengApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MengxiachengApplication.class, args);
+	}
+}
